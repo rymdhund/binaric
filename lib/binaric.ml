0fd0f924321e6b90765853526976f89988a4e6ba
rymdhund <7973243+rymdhund@users.noreply.github.com>
2019-03-27 21:27:03 +0000

    Initial commit

---
+++ lib/binaric.ml
@@ -0,0 +1,199 @@
+open Angstrom
+
+let is_digit = function
+  | '0'..'9' -> true
+  | _ -> false
+
+let is_alpha = function
+  | 'a'..'z' | 'A'..'Z' -> true
+  | _ -> false
+
+let is_name_char = function
+  | '_' | '-' -> true
+  | c -> (is_alpha c) || (is_digit c)
+
+let is_param_char = function
+  | 'a'..'z' | 'A'..'Z' | '0'..'9' -> true
+  | _ -> false
+
+let string_of_chars chars =
+  let buf = Buffer.create 16 in
+  List.iter (Buffer.add_char buf) chars;
+  Buffer.contents buf
+
+module Ast = struct
+  type value = [ `Value of string | `String of string ] [@@ deriving show]
+
+  module Segment = struct
+    type t = {
+      label: string option;
+      identifier: string;
+      parameters: value list;
+      multiplier: int option;
+    } [@@ deriving show]
+  end
+end
+
+module Parsers = struct
+  let escaped =
+    (char '\\') *> (satisfy (function | '"' | '\\' -> true | _ -> false))
+
+  let regular_string_char =
+    satisfy (function | '"' | '\\' -> false | _ -> true)
+
+  let quoted_char =
+    escaped <|> regular_string_char
+
+  let quoted_string =
+    (char '"') *> (many quoted_char) <* (char '"')
+    >>| (fun chars -> `String (string_of_chars chars))
+
+  let comment =
+    char '#' <* skip_while (function | '\x0a' | '\x0d' -> false | _ -> true) <* advance 1 >>| ignore
+
+  let ws = skip_while (function
+    | '\x20' | '\x0a' | '\x0d' | '\x09' -> true
+    | _ -> false
+  )
+
+  let optional_comment =
+    ws <* option () comment
+
+  let ws' =
+    optional_comment <* ws
+
+  let name =
+    ws *>
+    peek_char >>= function
+      | None -> fail "EOF, expected name"
+      | Some c ->
+        if is_alpha c
+        then take_while1 is_name_char
+        else fail (Format.sprintf "Invalid start of name: '%c'" c)
+
+  let label =
+    ws *> name <* ws <* (char ':') <* ws
+
+  let optional_label =
+    let some x = Some x in
+    option None (label >>| some)
+
+  let value =
+    lift2 (fun sign v -> `Value (sign ^ v))
+      (option "" (char '-' >>| (fun _ -> "-")))
+      (take_while1 is_param_char)
+
+  let param =
+    value <|> quoted_string
+
+  let single_parameter =
+    ws *> (char '|') *> ws *> param >>| (fun x -> [x])
+
+  let multi_parameter =
+    ws *> (char '[') *> (many (ws' *> param)) <* ws' <* (char ']')
+
+  let parameters =
+    single_parameter <|> multi_parameter <|> (return [])
+
+  let multiplier =
+    ws *> (char '*') *> ws *> (take_while1 is_digit) >>| int_of_string
+
+  let optional_multiplier =
+    let some x = Some x in
+    option None (multiplier >>| some)
+
+  let end_of_input =
+    peek_char >>= function
+    | Some c -> fail (Format.sprintf "Unexpected character: '%c'" c)
+    | None -> return ()
+
+  let segment =
+    lift4
+    (
+      fun label identifier parameters multiplier ->
+      Ast.Segment.{label; identifier; parameters; multiplier}
+    )
+    optional_label
+    name
+    parameters
+    optional_multiplier
+    <* optional_comment
+
+  let rec somefilter xs = match xs with
+    | [] -> []
+    | Some x :: tail -> x :: somefilter tail
+    | None :: tail -> somefilter tail
+
+  let program =
+    let some x = Some x in
+    let none _ = None in
+    many (
+      (segment >>| some)
+      <|>
+      (comment >>| none)
+    ) <* end_of_input >>| somefilter
+end
+
+module Eval = struct
+
+  let int_of_hex s =
+    try
+      Scanf.sscanf s "%x%!" (fun x -> Some x)
+    with Scanf.Scan_failure _ ->
+      None
+
+  let d8 v =
+    match v with
+    | `Value s -> (
+        match int_of_string_opt s with
+        | None -> Error (Format.sprintf "Invalid decimal number: %s" s)
+        | Some n when n > 255 || n < -254 -> Error (Format.sprintf "d8: %d is out of range" n)
+        | Some n -> Ok [char_of_int (n land 0xff)]
+    )
+    | `String s -> Error (Format.sprintf "d8 can not handle strings. It is being applied to \"%s\"" s)
+
+  let d16 v =
+    match v with
+    | `Value s -> (
+        match int_of_string_opt s with
+        | None -> Error (Format.sprintf "Invalid decimal number: %s" s)
+        | Some n when n > 0xffff || n <= -0xffff -> Error (Format.sprintf "d16: %d is out of range" n)
+        | Some n -> Ok [(char_of_int ((n lsr 8) land 0xff)); (char_of_int (n land 0xff))]
+    )
+    | `String s -> Error (Format.sprintf "d8 can not handle strings. It is being applied to \"%s\"" s)
+
+  let h8 v =
+    match v with
+    | `Value s -> (
+        match int_of_hex s with
+        | None -> Error (Format.sprintf "Invalid hex number: %s" s)
+        | Some n when n > 0xff || n < 0 -> Error (Format.sprintf "h8: %s is out of range" s)
+        | Some n -> Ok [char_of_int n]
+    )
+    | `String s -> Error (Format.sprintf "h8 can not handle strings. It is being applied to \"%s\"" s)
+
+  let rec result_map f xs =
+    match xs with
+    | [] -> Ok []
+    | x :: xs -> (
+      match f x, result_map f xs with
+      | Error s, _ -> Error s
+      | Ok _, Error s -> Error s
+      | Ok cs, Ok res -> Ok (List.append cs res)
+    )
+
+  let eval_seg seg =
+    match seg with
+    | Ast.Segment.{ identifier; parameters=[]; _ } -> Error (Format.sprintf "Unknown identifier \"%s\"" identifier)
+    | Ast.Segment.{ identifier; parameters; _ } ->
+        match identifier with
+        | "d8" -> result_map d8 parameters
+        | "h8" -> result_map h8 parameters
+        | "d16" -> result_map d16 parameters
+        | id -> Error (Format.sprintf "Unknown identifier '%s'" id)
+
+  let eval prog =
+    match prog with
+    | [] -> Ok []
+    | seg :: _ -> eval_seg seg
+end
---
+++ test/test.ml
@@ -0,0 +1,183 @@
+module Ast = Binaric.Ast
+module Parsers = Binaric.Parsers
+module Eval = Binaric.Eval
+
+let segment_eq (a:Ast.Segment.t) (b:Ast.Segment.t) =
+  a.label = b.label &&
+  a.identifier = b.identifier &&
+  a.parameters = b.parameters &&
+  a.multiplier = b.multiplier
+
+let segment =
+  let module M =
+    struct
+      type t = Ast.Segment.t
+      let pp = fun ppf seg ->
+        Format.fprintf ppf "%s" (Ast.Segment.show seg)
+      let equal = segment_eq
+    end
+  in
+  (module M: Alcotest.TESTABLE with type t = M.t)
+
+let check_parse_segment exp s () =
+  match Angstrom.parse_string Parsers.segment s with
+  | Error msg -> Alcotest.fail msg
+  | Ok seg ->
+    Alcotest.check segment "same segment" exp seg
+
+let check_parse exp s () =
+  match Angstrom.parse_string Parsers.program s with
+  | Error msg -> Alcotest.fail msg
+  | Ok seg ->
+    Alcotest.(check (list segment)) "same program" exp seg
+
+let mk_segment ?label ?multiplier identifier parameters =
+  Ast.Segment.{ label; multiplier; identifier; parameters }
+
+let segment_set = List.map
+  (fun (exp, s) -> ("parse segment", `Quick, check_parse_segment exp s))
+  [
+    (
+      mk_segment "abc" [],
+      "abc"
+    );
+    (
+      mk_segment ~label:"foo" "abc" [],
+      "foo: abc"
+    );
+    (
+      mk_segment ~label:"f-o_o" "a-b_c" [],
+      "f-o_o: a-b_c"
+    );
+    (
+      mk_segment ~label:"a" "b" [`Value "c"],
+      "a: b | c"
+    );
+    (
+      mk_segment "b" [],
+      "b [ ]"
+    );
+    (
+      mk_segment ~label:"a" "b" [`Value "c"],
+      "a: b [ c ]"
+    );
+    (
+      mk_segment ~label:"a" "b" [`Value "c"; `Value "d"],
+      "a: b [ c d ]"
+    );
+    (
+      mk_segment ~multiplier:10 "b" [],
+      "b * 10"
+    );
+    (
+      mk_segment ~label:"a" ~multiplier:2 "b" [`Value "c"],
+      "a: b | c * 2"
+    );
+  ]
+
+let program_set = List.map
+  (fun (exp, s) -> ("parse program", `Quick, check_parse exp s))
+  [
+    (
+      [
+        mk_segment "a" [];
+        mk_segment "b" [];
+      ],
+      "a\nb"
+    );
+    (
+      [
+        mk_segment "a" [];
+        mk_segment "b" [];
+      ],
+      "a # comment\nb"
+    );
+    (
+      [
+        mk_segment "a" [];
+      ],
+      "# comment\na"
+    );
+    (
+      [
+        mk_segment "a" [];
+      ],
+      "\na"
+    );
+    (
+      [
+        mk_segment "a" [ `Value "b"; `Value "c" ];
+      ],
+      "a [ b c ]"
+    );
+    (
+      [
+        mk_segment "a" [ `Value "b"; `Value "c" ];
+      ],
+      "a [ b #comment\n c ]"
+    );
+    (
+      [
+        mk_segment "a" [ `Value "b"; `Value "c" ];
+      ],
+      "a [ b #comment\n c ] \n "
+    );
+    (
+      [
+        mk_segment "a" [ `String "abc" ];
+      ],
+      "a | \"abc\" "
+    );
+    (
+      [
+        mk_segment "a" [ `String "\"" ];
+      ],
+      "a | \"\\\"\" "
+    );
+  ]
+
+let check_eval exp s () =
+  match Angstrom.parse_string Parsers.program s with
+  | Error msg -> Alcotest.fail msg
+  | Ok prog ->
+    match Eval.eval prog with
+    | Error msg -> Alcotest.fail msg
+    | Ok res -> Alcotest.(check (list char)) "same program" exp res
+
+let eval_set = List.map
+  (fun (exp, s) -> ("eval program", `Quick, check_eval exp s))
+  [
+    ([ '\x00' ], "d8 | 0 ");
+    ([ '\xff' ], "d8 | 255 ");
+    ([ '\xff' ], "d8 | -1 ");
+    ([ '\x00'; '\xff' ], "d8 [ 0 255 ] ");
+    ([ '\x00'; '\xff' ], "h8 [ 0 ff ] ");
+    ([ '\x00'; '\x01' ], "d16 [ 1 ] ");
+  ]
+
+let check_eval_fail expected s () =
+  match Angstrom.parse_string Parsers.program s with
+  | Error msg -> Alcotest.(check string) "same error" expected msg
+  | Ok prog ->
+    match Eval.eval prog with
+    | Error msg -> Alcotest.(check string) "same error" expected msg
+    | Ok _ -> Alcotest.fail "Expected to fail"
+
+let eval_fail_set = List.map
+  (fun (exp, s) -> ("eval fail program", `Quick, check_eval_fail exp s))
+  [
+    ("d8: 256 is out of range", "d8 | 256 ");
+    ("d8: -255 is out of range", "d8 | -255 ");
+    (": Unexpected character: '-'", "d8 | 1-1 ");
+    ("Invalid decimal number: 0f", "d8 | 0f ");
+    ("h8: 100 is out of range", "h8 | 100 ");
+    ("Invalid hex number: -1", "h8 | -1 ");
+  ]
+
+let () =
+  Alcotest.run "Binaric Tests" [
+    "segment_set", segment_set;
+    "program_set", program_set;
+    "eval_set", eval_set;
+    "eval_fail_set", eval_fail_set;
+  ]
