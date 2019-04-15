17483f84df304d1e5062aae22f6f1092724071e3
rymdhund <7973243+rymdhund@users.noreply.github.com>
2019-04-15 10:30:17 +0000

    Make command line interface

---
+++ bin/binaric.ml
@@ -0,0 +1,30 @@
+let main (outfile:string option) (force:bool) (infile:string): unit =
+
+  let drop_ending filename =
+    let len = CCString.length filename in
+    if len > 3 && CCString.suffix ~suf:".bn" filename then
+      Some (CCString.sub filename 0 (len-3))
+    else
+      None
+  in
+
+  let outfile = match outfile, drop_ending infile with
+  | Some f, _ -> f
+  | None, Some f -> f
+  | None, None -> "out.bin"
+  in
+
+  match Lib.eval_file infile outfile ~force with
+  | Ok () -> ()
+  | Error msg ->
+      Printf.eprintf "Error: %s\n" msg;
+      exit 1
+
+let () =
+  let open Lib.Args in
+  let prog = program main <*>
+    flag_param "o" "file" ~doc:"Output file" <*>
+    flag "f" ~alias:["force"] ~doc:"Force overwrite of output file" <*>
+    positional "source" ~doc:"Source .bn file to be compiled"
+  in
+  run prog
--- lib/binaric.ml
+++
@@ -1,6 +0,0 @@
-let (>>=) = CCResult.(>>=)
-let (>>|) = CCResult.(>|=)
-
-let run (prog:string): (string, string) result =
-  Angstrom.parse_string Parse.program prog >>= fun ast ->
-  Eval.eval_expression ast Eval.Env.empty >>| fun (_, output) -> Eval.Output.to_string output
